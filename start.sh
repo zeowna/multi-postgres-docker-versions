1aaf01fed48d007ba069817630c6d4370e89269e
José Chociai <jose.lucas@sousmile.com>
2025-09-01 21:12:00 -0300

    added --shm-size flag

--- start.sh
+++ start.sh
@@ -7,13 +7,14 @@ if [ ! -z $DOCKER_POSTGRES_PORT ]; then
 fi
 
 docker run \
+  --shm-size 1gb \
   -e "POSTGRES_USER=postgres" \
   -e "POSTGRES_PASSWORD=root" \
   -e "POSTGRES_HOST_AUTH_METHOD=trust" \
   -p $port:$port \
   -d \
   -v ~/docker/postgres/dumps:$HOME/docker/postgres/dumps \
-  -v ~/docker/postgres/data/$DOCKER_POSTGRES_VERSION:/var/lib/postgresql/data \
+  -v ~/docker/postgres/data/postgres-$DOCKER_POSTGRES_VERSION:/var/lib/postgresql/data \
   --name postgres-docker-$DOCKER_POSTGRES_VERSION-$port \
   postgres-docker-$DOCKER_POSTGRES_VERSION -p $port
 
